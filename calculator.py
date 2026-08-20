02bee6238c69ffded1b7861a0cedbb03647cee48
HectorMA06 <hectormulaagueda@gmail.com>
2026-08-20 14:53:38 +0200

    Add Files: Functions

--- calculator.py
+++ calculator.py
@@ -1,43 +1,31 @@
-def sum (a, b):
-    return a + b
-
-def rest(a, b):
-    return a - b
-
-
-def multiplication (a, b):
-    return a * b
-
-
-def divition (a, b):
-    return a / b
+import functions
 
 while True:
     try:
         print("This is a basic calculator using Python")
         a = int(input("Introduce the first number: "))
         b = int(input("Introduce the second number: "))
-        option = int(input(" - 1. Sum \n - 2. Rest \n - 3. Multiplication \n - 4. Divition \n Select the function: "))
+        option = int(input(" - 1. Sum \n - 2. Rest \n - 3. Multiplication \n - 4. Divition \n - 5. Module \n Select the function: "))
 
         if option == 1:
-            print(f"The sum of {a} + {b} is {sum(a,b)}")
+            print(f"The sum of {a} + {b} is {functions.sum(a,b)}")
             break
         
         elif option == 2:
-            print(f"The rest of {a} - {b} is {rest(a,b)}")
+            print(f"The rest of {a} - {b} is {functions.rest(a,b)}")
             break
         
         elif option == 3:
-            print(f"The multiplication of {a} * {b} is {multiplication(a,b)}")
+            print(f"The multiplication of {a} * {b} is {functions.multiplication(a,b)}")
             break
         
         elif option == 4:
-            if b == 0:
-                print(f"The divition of {a} / 1 is {divition(a,b)}")
-                break
-            else:
-                print(f"The divition of {a} / {b} is {divition(a,b)}")
-                break
+            print(f"The divition of {a} / {b} is {functions.divition(a,b)}")
+            break
+        
+        elif option == 5:
+            print(f"The module of {a} % {b} is {functions.module(a,b)}")
+            break
         else:
             print("Select a correct function \n")
             
---
+++ functions.py
@@ -0,0 +1,14 @@
+def sum (a, b):
+    return a + b
+
+def rest(a, b):
+    return a - b
+
+def multiplication (a, b):
+    return a * b
+
+def divition (a, b):
+    return a / b
+
+def module(a,b):
+    return a % b
