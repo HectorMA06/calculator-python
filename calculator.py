bb363d61cce97a1254ec6f18045d0f17aa0ada59
HectorMA06 <hectormulaagueda@gmail.com>
2026-08-20 15:14:51 +0200

    Features: Use dictionary

--- calculator.py
+++ calculator.py
@@ -7,25 +7,17 @@ while True:
         b = int(input("Introduce the second number: "))
         option = int(input(" - 1. Sum \n - 2. Rest \n - 3. Multiplication \n - 4. Divition \n - 5. Module \n Select the function: "))
 
-        if option == 1:
-            print(f"The sum of {a} + {b} is {functions.sum(a,b)}")
+        operators = {
+            1: functions.add,
+            2: functions.substract,
+            3: functions.multiplication,
+            4: functions.division,
+            5: functions.modulo,
+        }
+        if option in operators:
+            print(operators[option](a,b))
             break
-        
-        elif option == 2:
-            print(f"The rest of {a} - {b} is {functions.rest(a,b)}")
-            break
-        
-        elif option == 3:
-            print(f"The multiplication of {a} * {b} is {functions.multiplication(a,b)}")
-            break
-        
-        elif option == 4:
-            print(f"The divition of {a} / {b} is {functions.divition(a,b)}")
-            break
-        
-        elif option == 5:
-            print(f"The module of {a} % {b} is {functions.module(a,b)}")
-            break
+
         else:
             print("Select a correct function \n")
             
--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
-def sum (a, b):
-    return a + b
+def add (a, b):
+    return (f"The add of {a} + {b} is {a + b}")
 
-def rest(a, b):
-    return a - b
+def substract(a, b):
+    return (f"The substract of {a} - {b} is {a - b}")
 
 def multiplication (a, b):
-    return a * b
+    return (f"The multiplication of {a} * {b} is {a * b}")
 
-def divition (a, b):
-    return a / b
+def division (a, b):
+    return (f"The division of {a} / {b} is {a / b}")
 
-def module(a,b):
-    return a % b
+def modulo(a,b):
+    return (f"The modulo of {a} % {b} is {a % b}")
